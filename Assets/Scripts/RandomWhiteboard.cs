483a119b2d9661405dba53178e4dfb8bf189720c
FabBautis <FabBaut@gmail.com>
2022-12-02 22:14:48 -0700

    Starting work on posters for class
    Might make windows :shrug:

---
+++ Assets/Scripts/RandomWhiteboard.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWhiteboard : MonoBehaviour
+{
+    public GameObject whiteboard;
+    public Material[] whiteboardDesigns;
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteboard.GetComponent<Renderer>().material = whiteboardDesigns[Random.Range(0,whiteboardDesigns.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
